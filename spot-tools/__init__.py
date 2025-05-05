eef43b5794f60b0a23b2412679e154c7e5235dee
bguliano <72895782+bguliano@users.noreply.github.com>
2025-05-05 14:47:55 -0400

    rearrange items to form a package

---
+++ spot-tools/__init__.py
@@ -0,0 +1,10 @@
+from network_compute_client import NetworkComputeClient
+from network_compute_server import NetworkComputeServer
+from param_manager import ParamManager
+from rms_skeleton import RMSSkeleton
+from spot import Spot
+
+__version__ = '0.1.0'
+__author__ = 'Braden Guliano'
+
+__all__ = ['Spot', 'NetworkComputeServer', 'NetworkComputeClient', 'ParamManager', 'RMSSkeleton']
